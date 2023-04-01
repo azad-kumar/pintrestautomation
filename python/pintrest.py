c8849ec82c96b77352d4730bc8b7795f3fba932d
unknown <azadkumarw97@gmail.com>
2023-04-01 23:26:14 +0530

    started with pinterest upload

--- python/pintrest.py
+++ python/pintrest.py
@@ -1,12 +1,13 @@
 import files
 import driver_
 import json
+import time
 
 class upload_to_pintrest:
     url = 'https://in.pinterest.com/idea-pin-builder/'
 
     def __init__(self):
-        self.Driver = self.load_driver()
+        self.Driver = self.load_pintrest()
 
     def load_pintrest(self):
         driver_obj = driver_.driver_class()
@@ -19,3 +20,18 @@ class upload_to_pintrest:
         cookies_file = files.get_cookies_file()
         with open(cookies_file, 'w') as f:
             json.dump(cookies, f)
+
+    def login(self):
+        permission = input('should we start downloding cookies  (0/1) :')
+        if permission == 'y':
+            self.load_cookies()
+            time.sleep(10)
+            self.Driver.quit()
+        else:
+            self.Driver.quit()
+
+        
+
+if __name__ == '__main__':
+    pin_obj = upload_to_pintrest()
+    pin_obj.login()
