844ff8e827a447aea1c3158d3f4347721dc07214
unknown <azadkumarw97@gmail.com>
2023-04-02 09:46:03 +0530

    added getcookies.py

--- python/driver_.py
+++ python/driver_.py
@@ -25,19 +25,19 @@ class driver_class:
         # lol no idea why i created this function
         pass
 
-    # function should be calles before loading the driver
+    # function should be called before loading the driver
     def change_download_loc(self, download_dir):
         self.download_dir = download_dir
         self.chrome_options.add_experimental_option('prefs', {'download.default_directory': download_dir})
         return True
     
-    # function should be calles after loading the driver
+    # function should be called after loading the driver
     def add_cookies(self,cookies):
         self.cookies = cookies 
         for cookie in cookies:
             self.Driver.add_cookie(cookie)
 
-    # function should be calles before loading the driver
+    # function should be called before loading the driver
     def load_cookies(self, cookies_file):
         with open(cookies_file, 'r') as f:
             cookies = json.load(f)
